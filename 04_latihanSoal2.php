76c223e72ede1c6e256320e701236f4f5cbae824
rinusantoro <rinusantoro@gmail.com>
2024-03-20 11:04:52 +0700

    Added modul 4 latihan soal 2

---
+++ 04_latihanSoal2.php
@@ -0,0 +1,17 @@
+<?php
+
+//looping with for 
+/*for ($i = 15; $i <= 45; $i += 2) {
+echo "12 x $i = " . 12 * $i . "
+";
+}*/
+
+//use while 
+$i = 15;
+while ($i <= 45) {
+echo "12 x $i = " . 12 * $i . "
+";
+$i += 2;
+}
+
+?>
